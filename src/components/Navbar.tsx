66fd44cb0eb2c047fa0bc6fbfffea271d0ede85b
Donovan Gomez <donovanjuniogomez@gmail.com>
2023-04-24 20:16:39 -0700

    add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="relative flex pt-2 mx-auto w-5/6 text-sm font-medium text-stone-50">
+  <ul
+    className="w-full relative items-center mx-auto grid grid-cols-4 md:w-full lg:w-full"
+  >
+    <ul className="hidden md:flex md:gap-6 md:justify-start md:items-center">
+      <li className="md:justify-center md:items-center md:flex">
+        <Link href="/">
+          Logo
+        </Link>
+      </li>
+    </ul>
+    <ul
+      className="flex w-[22.55rem] justify-center md:flex md:col-span-3 md:justify-end md:w-full gap-6"
+    >
+      <li
+        className="flex justify-center text-xl items-center hover:text-violet-500 transition duration-75"
+      >
+        <Link href="/watchlist">Projects</Link>
+      </li>
+    </ul>
+  </ul>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
