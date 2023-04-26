892bf35ddbcd9c595f3a75c65ceff4e965eba633
Donovan Gomez <donovanjuniogomez@gmail.com>
2023-04-26 02:03:19 -0700

    add date-fns to homepage

--- src/components/AnimeList.tsx
+++ src/components/AnimeList.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { format, compareAsc } from "date-fns";
 import AnimeCard from "./AnimeCard";
 import AnimeContext from "~/context/AnimeContext";
 import WatchList from "./WatchList";
@@ -28,12 +29,20 @@ type AnimeProps = {
 
 const AnimeList: React.FC<AnimeProps> = ({ anime }) => {
   const { watching } = useContext(AnimeContext);
+  let today = format(new Date(), "EEEE");
   return (
     <section className="w-full border-2 border-blue-700">
       <div className="mx-auto w-11/12">
-        <ul className="flex flex-wrap gap-4">
+        {/* <ul className="flex flex-wrap gap-4">
           {anime.map((ani) => (
             <AnimeCard ani={ani} />
+          ))}
+        </ul> */}
+        <ul>
+          {anime.map((ani) => (
+            <div>
+              {ani.broadcast.day.includes(today) ? <AnimeCard ani={ani} /> : ""}
+            </div>
           ))}
         </ul>
       </div>
