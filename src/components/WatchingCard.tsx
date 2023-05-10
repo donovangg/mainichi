ca2b9ab78ce1e295ea592708181727c39a6b93a7
Donovan Gomez <donovanjuniogomez@gmail.com>
2023-05-10 01:18:55 -0700

    add cypress and playwright

--- src/components/WatchingCard.tsx
+++ src/components/WatchingCard.tsx
@@ -1,31 +1,45 @@
 import React from "react";
+import { FaTrash } from "react-icons/fa";
 
 type WatchingCardProps = {
   title: string;
   image_url: string;
-  url: string,
-  day: string,
-  timezone: string,
-  time: string
+  url: string;
+  day: string;
+  timezone: string;
+  time: string;
 };
 
-const WatchingCard: React.FC<WatchingCardProps> = ({ title, image_url, url, day, timezone, time }) => {
+const WatchingCard: React.FC<WatchingCardProps> = ({
+  title,
+  image_url,
+  url,
+  day,
+  timezone,
+  time,
+}) => {
   return (
     <div>
-      <li className="flex flex-col items-center rounded-lg border border-gray-200 bg-white shadow lg:w-[30rem] md:w-96 md:flex-row sm: w-96">
+      <li className="sm: flex w-96 flex-col items-center rounded-lg border relative border-gray-200 bg-white shadow md:w-96 md:flex-row lg:w-[30rem]">
         <img
           className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
           src={image_url}
           alt={title}
         />
+    <FaTrash className="absolute top-0 right-0 "/>
         <div className="flex flex-col justify-between p-4 leading-normal">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
             {title}
           </h5>
-           <p>{time}</p>
-           <p>{timezone}</p>
-           <p></p>
-           <a href={url}>Here</a>
+       
+          <div className="flex gap-1">
+          <span>{day}</span>
+            <span>{time}</span>
+            
+          </div>
+          <span>{timezone}</span>
+          <p></p>
+          <a href={url}>Here</a>
         </div>
       </li>
     </div>
