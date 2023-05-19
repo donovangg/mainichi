950e3d8b6f620d4f7dda97c9fd5f7cfb439f0dee
Donovan <donovanjuniogomez@gmail.com>
2023-05-19 02:07:05 -0700

    add ignore eslint on config file

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,9 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
