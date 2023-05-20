6358ee4bd3f6ca258059f963d5460e832cb836c6
Donovan <donovanjuniogomez@gmail.com>
2023-05-19 20:25:08 -0700

    style navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import Link from "next/link";
 import { FaBookmark } from "react-icons/fa";
 import { useSession, signOut, signIn } from "next-auth/react";
-import Dropdown from "./Dropdown";
 
 const Navbar = () => {
   const { data: session } = useSession();
   return (
     <>
-      <nav className="flex flex-wrap items-center justify-between border-t-2 border-solid border-blue-700 bg-white py-4 shadow lg:px-12">
-        <div className="flex w-full justify-between border-b-2 border-solid border-gray-300 pb-5 pl-6 pr-2 lg:w-auto lg:border-b-0 lg:pb-0">
-          <div className="mr-16 flex flex-shrink-0 items-center text-gray-800 border-2 border-red-500">
+      <nav className="flex w-full  border-2 border-solid  bg-white shadow ">
+        <div className="border-2 mx-auto relative grid grid-cols-2 w-10/12 border-purple-800">
+        <div className="flex justify-between border-2 border-solid border-gray-300 pb-5  pr-2 lg:w-auto lg:border-b-0 lg:pb-0">
+          <div className="mr-16 ml-0 flex flex-shrink-0 items-center text-gray-800 border-2 border-red-500">
             <span className="text-xl font-semibold tracking-tight">
               mainichi
             </span>
           </div>
-          <div className="block border-2 boreder-green-500 lg:hidden ">
-            {/* dropdown breaks lol */}
-            {/* <Dropdown /> */}
+          <div className="block border-2 border-green-500 sm:hidden ">
             <Link
               href="/"
               className=" mr-2 mt-4 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
@@ -36,8 +34,8 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="menu w-full flex-grow px-8 lg:flex lg:w-auto lg:items-center lg:px-3 md:hidden ">
-          <div className="text-md font-bold text-blue-700 lg:flex-grow md:hidden sm:hidden">
+        <div className="menu hidden border-2 lg:col-start-4 lg:left-0 lg:gap-4  border-orange-800 lg:flex  lg:items-center  md:hidden sm:hidden ">
+          <div className="text-md font-bold text-blue-700 lg:flex-grow md: ">
             <Link
               href="/"
               className=" mr-2 mt-4 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
@@ -61,6 +59,7 @@ const Navbar = () => {
               <button onClick={() => signOut()}>Sign Out</button>
             )}
           </div>
+        </div>
         </div>
       </nav>
     </>
