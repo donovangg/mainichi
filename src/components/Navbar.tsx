9d6e618f9cd2deebb5d5cce661a90e2043f9aabe
Donovan <donovanjuniogomez@gmail.com>
2023-05-23 00:17:45 -0700

    add MAL icon, Favicon and Navbar home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,58 +8,60 @@ const Navbar = () => {
   return (
     <>
       <nav className="flex w-full   bg-white shadow ">
-        <div className="mx-auto relative grid grid-cols-2 w-10/12 pt-2 pb-2 ">
-        <div className="flex items-center justify-between   pr-2 lg:w-auto lg:border-b-0 ">
-          <div className="mr-16 ml-0 flex flex-shrink-0 items-center text-gray-800">
-            <span className="text-xl font-semibold tracking-tight">
-              mainichi
-            </span>
+        <div className="relative mx-auto grid w-10/12 grid-cols-2 pb-2 pt-2 ">
+          <div className="flex items-center justify-between   pr-2 lg:w-auto lg:border-b-0 ">
+            <div className="ml-0 mr-16 flex flex-shrink-0 items-center text-gray-800">
+              <Link href="/">
+                <span className="text-xl font-semibold tracking-tight">
+                  mainichi
+                </span>
+              </Link>
+            </div>
+            <div className=" flex h-full items-center gap-2  sm:hidden ">
+              <Link
+                href="/"
+                className=" mr-2 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
+              >
+                Schedule
+              </Link>
+              <Link
+                href="/watchlist"
+                className="mr-2  flex flex-row rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
+              >
+                <div className="flex items-center">
+                  <span className="mx-1">Watching</span>
+                  <FaBookmark />{" "}
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className=" flex gap-2 h-full items-center  sm:hidden ">
-            <Link
-              href="/"
-              className=" mr-2 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
-            >
-              Schedule
-            </Link>
-            <Link
-              href="/watchlist"
-              className="mr-2  flex flex-row rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
-            >
-              <div className="flex items-center">
-                <span className="mx-1">Watching</span>
-                <FaBookmark />{" "}
-              </div>
-            </Link>
-          </div>
-        </div>
 
-        <div className="menu hidden md:flex md:col-start-4  lg:col-start-4 lg:left-0 lg:gap-4  lg:flex  lg:items-center  sm:hidden ">
-          <div className="text-md font-bold text-blue-700 md:flex md:items-center lg:flex-grow md: ">
-            <Link
-              href="/"
-              className=" mr-2 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
-            >
-              Schedule
-            </Link>
-            <Link
-              href="/watchlist"
-              className="mr-2 flex flex-row rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
-            >
-              <div className="flex items-center">
-                <span className="mx-1">Watching</span>
-                <FaBookmark />{" "}
-              </div>
-            </Link>
+          <div className="menu hidden sm:hidden md:col-start-4  md:flex lg:left-0 lg:col-start-4  lg:flex  lg:items-center  lg:gap-4 ">
+            <div className="text-md md: font-bold text-blue-700 md:flex md:items-center lg:flex-grow ">
+              <Link
+                href="/"
+                className=" mr-2 block rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
+              >
+                Schedule
+              </Link>
+              <Link
+                href="/watchlist"
+                className="mr-2 flex flex-row rounded px-4 py-2 hover:bg-blue-700 hover:text-white lg:mt-0 lg:inline-block"
+              >
+                <div className="flex items-center">
+                  <span className="mx-1">Watching</span>
+                  <FaBookmark />{" "}
+                </div>
+              </Link>
+            </div>
+            <div className="flex ">
+              {!session ? (
+                <button onClick={() => signIn()}>Sign In</button>
+              ) : (
+                <button onClick={() => signOut()}>Sign Out</button>
+              )}
+            </div>
           </div>
-          <div className="flex ">
-            {!session ? (
-              <button onClick={() => signIn()}>Sign In</button>
-            ) : (
-              <button onClick={() => signOut()}>Sign Out</button>
-            )}
-          </div>
-        </div>
         </div>
       </nav>
     </>
--- src/components/WatchingCard.tsx
+++ src/components/WatchingCard.tsx
@@ -25,7 +25,7 @@ const WatchingCard: React.FC<WatchingCardProps> = ({
   const { addWatching, watching, deleteWatching } = useContext(AnimeContext);
   return (
     <div key={mal_id}>
-      <li className="sm: relative flex w-80 flex-col items-center rounded-lg border border-gray-200 bg-white shadow md:w-96 md:flex-row lg:w-[30rem]">
+      <li className="sm: relative flex w-96 flex-col items-center rounded-lg border border-gray-200 bg-white shadow md:w-96 md:flex-row lg:w-[30rem]">
         <img
           className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
           src={image_url}
@@ -36,7 +36,7 @@ const WatchingCard: React.FC<WatchingCardProps> = ({
             deleteWatching(mal_id);
           }}
         >
-          <FaTrash className="absolute right-0 top-0 text-3xl" />
+          <FaTrash className="absolute right-2 top-2 text-2xl" />
         </button>
         <div className="flex flex-col justify-between p-4 leading-normal">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
@@ -48,7 +48,9 @@ const WatchingCard: React.FC<WatchingCardProps> = ({
             <span>{time}</span>
           </div>
           <span>{timezone}</span>
-          <a href={url}>Here</a>
+          <a href={url} target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.273 7.247v8.423l-2.103-.003v-5.216l-2.03 2.404l-1.989-2.458l-.02 5.285H.001L0 7.247h2.203l1.865 2.545l2.015-2.546l2.19.001zm8.628 2.069l.025 6.335h-2.365l-.008-2.871h-2.8c.07.499.21 1.266.417 1.779c.155.381.298.751.583 1.128l-1.705 1.125c-.349-.636-.622-1.337-.878-2.082a9.296 9.296 0 0 1-.507-2.179c-.085-.75-.097-1.471.107-2.212a3.908 3.908 0 0 1 1.161-1.866c.313-.293.749-.5 1.1-.687c.351-.187.743-.264 1.107-.359a7.405 7.405 0 0 1 1.191-.183c.398-.034 1.107-.066 2.39-.028l.545 1.749H14.51c-.593.008-.878.001-1.341.209a2.236 2.236 0 0 0-1.278 1.92l2.663.033l.038-1.81h2.309zm3.992-2.099v6.627l3.107.032l-.43 1.775h-4.807V7.187l2.13.03z"/></svg>
+          </a>
 
         </div>
       </li>
