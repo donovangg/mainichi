4dd2ff0806377d8f19593d954b15d6866ed01cd6
Donovan <donovanjuniogomez@gmail.com>
2023-06-28 01:49:09 -0700

    add error handling to login form for wrong password, signup is just alert() for now

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -8,6 +8,7 @@ const LoginForm = () => {
   const { signedInUser, logIn } = UserAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const router = useRouter();
 
   const handleSubmit = async (e) => {
@@ -16,19 +17,21 @@ const LoginForm = () => {
       await logIn(email, password)
       router.push("/account")
     } catch(error){
-        console.log(error);
+        console.log(error.message);
+        setError(error.message);
     }
   }
 
   return (
     <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-      <div className="w-full rounded-lg bg-white shadow dark:border  sm:max-w-md md:mt-0 xl:p-0">
+      <div className="w-full rounded-lg bg-white shadow dark:border  sm:max-w-lg md:mt-0 xl:p-0">
         <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900  md:text-2xl">
-            Create and account
+            Login
           </h1>
           <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
             <div>
+              {error ? <span className="text-red-700">{error}.</span> : ""}
               <label
                 htmlFor="email"
                 className="mb-2 block text-sm font-medium text-gray-900 "
@@ -66,15 +69,15 @@ const LoginForm = () => {
               type="submit"
               className="hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-pink-600 px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
             >
-              Create an account
+              Login
             </button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Already have an account?{" "}
+            Don&#39;t have an account?{" "}
               <Link
-                href="/login"
+                href="/signup"
                 className="text-primary-600  dark:text-primary-500 font-medium hover:underline"
               >
-                Login here
+                Signup
               </Link>
             </p>
           </form>
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { useContext, createContext, useEffect, useState } from 'react';
+import { useContext, createContext, useEffect, useState } from "react";
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -7,22 +7,22 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   sendSignInLinkToEmail,
-  UserCredential
-} from 'firebase/auth';
+  UserCredential,
+} from "firebase/auth";
 
-import { auth } from '~/firebase/firebase';
-import { doc, setDoc } from "firebase/firestore"; 
-import   {db}   from '../firebase/firebase'
+import { auth } from "~/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 interface AuthUserContext {
-    signInWithGoogle: () => void;
-    logOut: () => void;
-    signUp(email: any, password: any): void
-    logIn: (email: any, password: any) => Promise<UserCredential>;
-    signedInUser:  {
-        displayName?: string
-        email?: string
-    }
+  signInWithGoogle: () => void;
+  logOut: () => void;
+  signUp(email: any, password: any): void;
+  logIn: (email: any, password: any) => Promise<UserCredential>;
+  signedInUser: {
+    displayName?: string;
+    email?: string;
+  };
 }
 
 const AuthContext = createContext<AuthUserContext>({} as AuthUserContext);
@@ -31,50 +31,63 @@ export const AuthContextProvider = ({ children }) => {
   const [signedInUser, setSignedInUser] = useState({});
 
   function signUp(email, password) {
-    createUserWithEmailAndPassword(auth, email, password);
-
-      setDoc(doc(db, "users", email), {
-        savedAnime: []
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        alert(errorMessage)
+        // ..
       });
+
+    setDoc(doc(db, "users", email), {
+      savedAnime: [],
+    });
   }
 
   function logIn(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
   }
 
-const signInWithGoogle = () => {
+  const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      const name = result.user.displayName;
-      const email = result.user.email;
-      const profilePic = result.user.photoURL;
-      console.log(user)
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-}
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        const name = result.user.displayName;
+        const email = result.user.email;
+        const profilePic = result.user.photoURL;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   const logOut = () => {
-      signOut(auth)
-  }
+    signOut(auth);
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setSignedInUser(currentUser);
-      console.log('Hello User', currentUser)
+      console.log("Hello User", currentUser);
     });
     return () => {
       unsubscribe();
@@ -82,7 +95,9 @@ const signInWithGoogle = () => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signInWithGoogle, logOut, signedInUser, signUp, logIn }}>
+    <AuthContext.Provider
+      value={{ signInWithGoogle, logOut, signedInUser, signUp, logIn }}
+    >
       {children}
     </AuthContext.Provider>
   );
