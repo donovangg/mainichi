2e413c5c85a851ffe1a7a1ca22df980f5e06eaae
Donovan <donovanjuniogomez@gmail.com>
2023-07-15 01:18:54 -0700

    dynamically render header image

--- src/pages/anime/[id].tsx
+++ src/pages/anime/[id].tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 import type {
   GetStaticPaths,
   GetStaticProps,
-  InferGetStaticPropsType
-} from 'next'
+  InferGetStaticPropsType,
+} from "next";
 
 export const getStaticPaths = async () => {
-  const res = await fetch('https://api.jikan.moe/v4/seasons/2023/summer');
+  const res = await fetch("https://api.jikan.moe/v4/seasons/2023/summer");
   const data = await res.json();
   const anime = data.data;
 
@@ -15,32 +15,41 @@ export const getStaticPaths = async () => {
     paths: anime.map((ani) => {
       return {
         params: {
-          id: ani.mal_id.toString()
-        }
-      }
+          id: ani.mal_id.toString(),
+        },
+      };
     }),
-    fallback:false
-  }
-}
+    fallback: false,
+  };
+};
 
-export const getStaticProps = async ({params}) => {
+export const getStaticProps = async ({ params }) => {
   const id = params.id;
   const res = await fetch(`https://api.jikan.moe/v4/anime/${id}`);
   const data = await res.json();
 
   return {
-    props: {ani: data}
-  }
-}
+    props: { ani: data },
+  };
+};
 
-const AnimeDetails = ({ani}) => {
+const AnimeDetails = ({ ani }) => {
   const router = useRouter();
-console.log(ani.data);
+  console.log(ani.data);
+  const imageUrl = ani.data.trailer.images.maximum_image_url;
   return (
-   <div>
-    <h1 className="text-red-700">{ani.data.title}</h1>
-   </div>
-  )
-}
+    <div>
+      <header
+        className={`relative flex w-screen flex-col  items-center overflow-hidden
+     bg-[image:var(--image-url)] bg-cover bg-no-repeat py-32`}
+     style={{backgroundImage:`url(${imageUrl})`}} 
+      >
+        <div className="absolute left-0 top-0 h-full w-full bg-[url('/assets/texture.png')]"></div>
+        <div className="absolute left-0 top-0 h-full w-full bg-black opacity-40"></div>
+      </header>
+      <h1 className="text-red-700">{ani.data.title}</h1>
+    </div>
+  );
+};
 
-export default AnimeDetails
+export default AnimeDetails;
